645389dd8af1d1bc26811682a91f108c8fc3671b
InuKikyo <inukikyo.17@gmail.com>
2026-06-15 03:18:25 -0300

    Cargar países
    Cambiamos la versión de prueba por la versión que va a ir quedando

--- TPI_Paises.py
+++ TPI_Paises.py
@@ -3,18 +3,17 @@ import csv
 def cargar_paises():
     paises = []
 
-    with open("paises.csv", mode = "r", encoding = "utf-8") as archivo:
-        lector = csv.DictReader(archivo)
+    try:
+        with open("paises.csv", mode = "r", encoding = "utf-8") as archivo:
+            lector = csv.DictReader(archivo)
 
-        for fila in lector:
-            pais = {"nombre": fila["nombre"],
+            for fila in lector:
+                paises.append({"nombre": fila["nombre"],
                     "poblacion": int(fila["poblacion"]),
                     "superficie": int(fila["superficie"]),
-                    "continente": fila["continente"]}
-            paises.append(pais)
+                    "continente": fila["continente"]})
+    except FileNotFoundError:
+        print ("Archivo csv no encontrado.")
+
     return paises
-
-paises = cargar_paises()
-
-for pais in paises:
-    print (pais)
+    
